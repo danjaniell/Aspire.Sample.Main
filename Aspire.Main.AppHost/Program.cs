acaff80c23c93e94608dd85c9bed57243de3732d
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-21 11:32:22 +0800

    Default for new branch

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,13 +1,3 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
-
-var database = builder.AddProject<Infrastructure_DB_InMemory>("db");
-
-var apiservice = builder.AddProject<Weather_API>("apiservice")
-       .WithReference(database);
-
-builder.AddProject<Weather_UI>("webfrontend")
-       .WithReference(apiservice);
 
 builder.Build().Run();
