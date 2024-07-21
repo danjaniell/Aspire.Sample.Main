e06ff6a859f4a5fe3a8ec5f48835029df2fe16c7
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 01:41:22 +0800

    Docker works

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infrastructure", "master")
+       .WithHttpEndpoint(port: 7227, targetPort: 8080);
+builder.AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
+       .WithHttpEndpoint(port: 7017, targetPort: 8080)
+       .WithEnvironment("DBUrl", "http://192.168.1.246:7227");
+builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui","master")
+       .WithHttpEndpoint(port: 7016, targetPort: 8080)
+       .WithEnvironment("APIUrl", "http://aspire-sample-api:8080");
+
 builder.Build().Run();
