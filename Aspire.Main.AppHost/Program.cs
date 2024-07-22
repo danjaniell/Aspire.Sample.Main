7c29a5320a314c017dbf8942a3ced58e5be4e2b2
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 18:25:15 +0800

    Use docker service name

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -4,9 +4,9 @@ builder.AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infra
        .WithHttpEndpoint(port: 7227, targetPort: 8080, name: "aspire-sample-db");
 builder.AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
        .WithHttpEndpoint(port: 7017, targetPort: 8080, name: "aspire-sample-api")
-       .WithEnvironment("DBUrl", "http://localhost:7227");
+       .WithEnvironment("DBUrl", "http://aspire-sample-db:7227");
 builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui", "master")
        .WithHttpEndpoint(port: 7016, targetPort: 8080, name: "aspire-sample-ui")
-       .WithEnvironment("APIUrl", "http://localhost:7017");
+       .WithEnvironment("APIUrl", "http://aspire-sample-api:7017");
 
 builder.Build().Run();
