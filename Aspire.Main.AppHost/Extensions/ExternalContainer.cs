e13cb658784617811f39055f934e7bb9eac8062a
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 18:06:44 +0800

    Still can't locate docker service

---
+++ Aspire.Main.AppHost/Extensions/ExternalContainer.cs
@@ -0,0 +1,80 @@
+﻿using Aspire.Hosting.Lifecycle;
+using CliWrap.EventStream;
+using CliWrap;
+using Microsoft.Extensions.Logging;
+
+namespace Aspire.Main.AppHost.Extensions;
+
+internal sealed class ExternalContainerResource(string name, string containerNameOrId) : Resource(name)
+{
+    public string ContainerNameOrId { get; } = containerNameOrId;
+}
+
+internal static class ExternalContainerResourceExtensions
+{
+    public static IResourceBuilder<ExternalContainerResource> AddExternalContainer(this IDistributedApplicationBuilder builder, string name, string containerNameOrId)
+    {
+        builder.Services.TryAddLifecycleHook<ExternalContainerResourceLifecycleHook>();
+
+        return builder.AddResource(new ExternalContainerResource(name, containerNameOrId))
+            .WithInitialState(new CustomResourceSnapshot
+            {
+                ResourceType = "External container",
+                State = "Starting",
+                Properties = [new ResourcePropertySnapshot(CustomResourceKnownProperties.Source, "Custom")]
+            })
+            .ExcludeFromManifest();
+    }
+}
+
+internal sealed class ExternalContainerResourceLifecycleHook(ResourceNotificationService notificationService, ResourceLoggerService loggerService)
+    : IDistributedApplicationLifecycleHook, IAsyncDisposable
+{
+    private readonly CancellationTokenSource _tokenSource = new();
+
+    public Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
+    {
+        foreach (var resource in appModel.Resources.OfType<ExternalContainerResource>())
+        {
+            this.StartTrackingExternalContainerLogs(resource, this._tokenSource.Token);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void StartTrackingExternalContainerLogs(ExternalContainerResource resource, CancellationToken cancellationToken)
+    {
+        var logger = loggerService.GetLogger(resource);
+
+        _ = Task.Run(async () =>
+        {
+            var cmd = Cli.Wrap("docker").WithArguments(["logs", "--follow", resource.ContainerNameOrId]);
+            var cmdEvents = cmd.ListenAsync(cancellationToken);
+
+            await foreach (var cmdEvent in cmdEvents)
+            {
+                switch (cmdEvent)
+                {
+                    case StartedCommandEvent:
+                        await notificationService.PublishUpdateAsync(resource, state => state with { State = "Running" });
+                        break;
+                    case ExitedCommandEvent:
+                        await notificationService.PublishUpdateAsync(resource, state => state with { State = "Finished" });
+                        break;
+                    case StandardOutputCommandEvent stdOut:
+                        logger.LogInformation("External container {ResourceName} stdout: {StdOut}", resource.Name, stdOut.Text);
+                        break;
+                    case StandardErrorCommandEvent stdErr:
+                        logger.LogInformation("External container {ResourceName} stderr: {StdErr}", resource.Name, stdErr.Text);
+                        break;
+                }
+            }
+        }, cancellationToken);
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        this._tokenSource.Cancel();
+        return default;
+    }
+}
--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infrastructure", "master")
-       .WithHttpEndpoint(port: 7227, targetPort: 8080);
+       .WithHttpEndpoint(port: 7227, targetPort: 8080, name: "aspire-sample-db");
 builder.AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
-       .WithHttpEndpoint(port: 7017, targetPort: 8080)
+       .WithHttpEndpoint(port: 7017, targetPort: 8080, name: "aspire-sample-api")
        .WithEnvironment("DBUrl", "http://localhost:7227");
 builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui", "master")
-       .WithHttpEndpoint(port: 7016, targetPort: 8080)
+       .WithHttpEndpoint(port: 7016, targetPort: 8080, name: "aspire-sample-ui")
        .WithEnvironment("APIUrl", "http://localhost:7017");
 
 builder.Build().Run();
