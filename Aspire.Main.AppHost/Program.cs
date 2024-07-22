f9642b2ed1cdc693e095b6fad7a9f0de9891ac54
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 14:13:24 +0800

    Configs change

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -4,9 +4,9 @@ builder.AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infra
        .WithHttpEndpoint(port: 7227, targetPort: 8080);
 builder.AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
        .WithHttpEndpoint(port: 7017, targetPort: 8080)
-       .WithEnvironment("DBUrl", "http://192.168.1.246:7227");
-builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui","master")
+       .WithEnvironment("DBUrl", "http://localhost:7227");
+builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui", "master")
        .WithHttpEndpoint(port: 7016, targetPort: 8080)
-       .WithEnvironment("APIUrl", "http://aspire-sample-api:8080");
+       .WithEnvironment("APIUrl", "http://localhost:7017");
 
 builder.Build().Run();
