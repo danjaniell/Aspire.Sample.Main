f8600bd45a27d615bf103ca9ed81fc7c7faecd9b
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 15:09:27 +0800

    Try include Seq Logging

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,4 +1,5 @@
 using Projects;
+using System.Net.Sockets;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -9,5 +10,11 @@ var apiservice = builder.AddProject<Weather_API>("apiservice")
 
 builder.AddProject<Weather_UI>("webfrontend")
        .WithReference(apiservice);
+
+builder
+    .AddResource(new ContainerResource("seq"))
+    .WithAnnotation(new EndpointAnnotation(ProtocolType.Tcp, uriScheme: "http", name: "seq", port: 5341, targetPort: 80))
+    .WithEnvironment("ACCEPT_EULA", "Y")
+    .WithAnnotation(new ContainerImageAnnotation { Image = "datalust/seq", Tag = "latest" });
 
 builder.Build().Run();
