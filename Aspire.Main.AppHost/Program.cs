2ac1f8f58ca72ca8802f5eac2ae5e4228dc7354d
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-22 14:46:49 +0800

    Added retrieval of source from git repo

--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,3 +1,35 @@
+using Dutchskull.Aspire.PolyRepo;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("redis");
+
+var dbRepo = builder.AddRepository(
+    "repository",
+    "https://github.com/danjaniell/Aspire.Sample.Infrastructure.git",
+    c => c.WithDefaultBranch("local")
+          .WithTargetPath("../../repos"));
+
+var apiRepo = builder.AddRepository(
+    "repository",
+    "https://github.com/danjaniell/Aspire.Sample.API.git",
+    c => c.WithDefaultBranch("local")
+          .WithTargetPath("../../repos"));
+
+var uiRepo = builder.AddRepository(
+    "repository",
+    "https://github.com/danjaniell/Aspire.Sample.UI.git",
+    c => c.WithDefaultBranch("local")
+          .WithTargetPath("../../repos"));
+
+var db = builder
+    .AddProjectFromRepository("aspire-sample-db", dbRepo,
+        "Infrastructure.DB.InMemory/*csproj");
+var api = builder
+    .AddProjectFromRepository("aspire-sample-api", apiRepo,
+        "Weather.API/Weather.API.csproj");
+var ui = builder
+    .AddProjectFromRepository("aspire-sample-ui", uiRepo,
+        "Weather.UI/Weather.UI.csproj");
 
 builder.Build().Run();
