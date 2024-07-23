79174d3a50e24caa90e833321b58721c50ee6741
Jai Paragas <iaj.paragas@yahoo.com>
2024-07-23 14:43:48 +0800

    Docker works, using internal ip and host network

---
+++ Aspire.Main.AppHost/Configurations.cs
@@ -0,0 +1,7 @@
+﻿namespace Aspire.Main.AppHost;
+
+public static class Configurations
+{
+    public static string GetDockerHostIp() =>
+        Environment.GetEnvironmentVariable("DOCKER_HOST_IP") ?? string.Empty;
+}
--- Aspire.Main.AppHost/Program.cs
+++ Aspire.Main.AppHost/Program.cs
@@ -1,12 +1,25 @@
+using Aspire.Main.AppHost;
+
 var builder = DistributedApplication.CreateBuilder(args);
+string dockerHostIp = Configurations.GetDockerHostIp();
 
-builder.AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infrastructure", "master")
-       .WithHttpEndpoint(port: 7227, targetPort: 8080, name: "aspire-sample-db");
-builder.AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
-       .WithHttpEndpoint(port: 7017, targetPort: 8080, name: "aspire-sample-api")
-       .WithEnvironment("DBUrl", "http://aspire-sample-db:7227");
-builder.AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui", "master")
-       .WithHttpEndpoint(port: 7016, targetPort: 8080, name: "aspire-sample-ui")
-       .WithEnvironment("APIUrl", "http://aspire-sample-api:7017");
+builder
+    .AddContainer("aspire-sample-db", "ghcr.io/danjaniell/aspire.sample.infrastructure", "master")
+    //.WithEndpoint(scheme: "http", port: 7227, targetPort: 1818, name: "aspire-sample-db", isProxied: false, isExternal: true)
+    .WithHttpEndpoint(port: 7227, targetPort: 1818, name: "aspire-sample-db", isProxied: false)
+    .WithContainerRuntimeArgs("--net=host");
+builder
+    .AddContainer("aspire-sample-api", "ghcr.io/danjaniell/aspire.sample.api", "master")
+    //.WithEndpoint(scheme: "http", port: 7017, targetPort: 1919, name: "aspire-sample-api", isProxied: false, isExternal: true)
+    .WithHttpEndpoint(port: 7017, targetPort: 1919, name: "aspire-sample-api", isProxied: false)
+    .WithEnvironment("DBUrl", $"http://{dockerHostIp}:1818")
+    .WithContainerRuntimeArgs("--net=host");
+builder
+    .AddContainer("aspire-sample-ui", "ghcr.io/danjaniell/aspire.sample.ui", "master")
+    //.WithEndpoint(scheme: "http", port: 7016, targetPort: 2020, name: "aspire-sample-ui", isProxied: false, isExternal: true)
+    .WithHttpEndpoint(port: 7016, targetPort: 2020, name: "aspire-sample-ui", isProxied: false)
+    .WithEnvironment("APIUrl", $"http://{dockerHostIp}:1919")
+    .WithContainerRuntimeArgs("--net=host");
 
-builder.Build().Run();
+var app = builder.Build();
+await app.RunAsync();
